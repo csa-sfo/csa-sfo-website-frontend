639e2327103a259fc3b98cfc0a315341740da2fb
Keerthika <keerthi.yerabolu@indrasol.com>
2026-01-07 13:45:38 -0800

    Fetching images from Google Drive

--- src/config/api.ts
+++ src/config/api.ts
@@ -38,6 +38,8 @@ export const API_ENDPOINTS = {
   UPLOAD_IMAGE: `${API_BASE_URL}/v1/routes/upload/image`,
   // Event Images Endpoints
   LIST_EVENT_IMAGES: `${API_BASE_URL}/v1/routes/event-images`,
+  // Gallery Images Endpoints (separate from event-images which is for slideshow)
+  GALLERY_IMAGES: `${API_BASE_URL}/v1/routes/gallery-images`,
   // Volunteer Endpoints
   VOLUNTEER_SUBMIT: `${API_BASE_URL}/v1/routes/volunteers/submit`,
   VOLUNTEER_ALL: `${API_BASE_URL}/v1/routes/volunteers/all`,
